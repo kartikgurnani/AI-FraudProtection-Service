d95d55491e39cd4c874c39d1c6cff8cbd943615d
KARTIK GURNANI <kartikgurnani@users.noreply.github.com>
2026-02-26 00:24:27 +0530

    Add Kafka configuration for broker and topics

---
+++ Kafka/schemas/kafka_config.py
@@ -0,0 +1,8 @@
+KAFKA_BROKER = "localhost:9092"
+
+TOPICS = {
+    "build": "build_metadata",
+    "deployment": "model_deployment",
+    "fraud_version": "fraud_model_versions",
+    "dlq": "dead_letter_queue"
+}
